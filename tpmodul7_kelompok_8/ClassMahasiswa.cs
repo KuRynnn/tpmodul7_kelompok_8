4e0b262ea2e0f677f1906fc9b49a24bc05e75903
rasyeeed <muhammadnurrasyid@gmail.com>
2023-04-02 18:58:16 +0700

    menambahkan class DataMahasiswa_1302213039

---
+++ tpmodul7_kelompok_8/ClassMahasiswa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul7_kelompok_8
+{
+    internal class ClassMahasiswa
+    {
+        public Nama nama { get; set; }
+        public int nim { get; set; }
+        public string fakultas { get; set; }
+        public ClassMahasiswa(Nama nama, int nim, string fakultas)
+        {
+            this.nama = nama;
+            this.nim = nim;
+            this.fakultas = fakultas;
+        }
+    }
+}
---
+++ tpmodul7_kelompok_8/DataMahasiswa_1302213039.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace tpmodul7_kelompok_8
+{
+    internal class DataMahasiswa_1302213039
+    {
+        public void ReadJSON()
+        {
+            string jsonString = File.ReadAllText("C:\\Telkom\\Semester 4\\KPL\\tp7\\tpmodul7_kelompok_8\\tp7_1_1302213039.json");
+            ClassMahasiswa mahasiswa = JsonSerializer.Deserialize<ClassMahasiswa>(jsonString);
+            Console.WriteLine("Nama " + mahasiswa.nama.depan +" "+ mahasiswa.nama.belakang + " dengan nim " + mahasiswa.nim + " dari fakultas " + mahasiswa.fakultas);
+        }
+    }
+}
