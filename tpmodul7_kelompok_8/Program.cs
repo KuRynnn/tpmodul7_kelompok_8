e67cf544f0af730204e47e86ddd24661fabcdaa2
Drakkura <gonhgyhjj1@gmail.com>
2023-04-02 18:47:25 +0700

    menambahkan class KuliahMahasiswa1302210124

--- tpmodul7_kelompok_8/Program.cs
+++ tpmodul7_kelompok_8/Program.cs
@@ -3,12 +3,12 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        DataMahasiswa1302210124 fadel = new DataMahasiswa1302210124();
-        fadel.ReadJSON();
+        DataMahasiswa1302210124 ryan = new DataMahasiswa1302210124();
+        ryan.ReadJSON();
 
         Console.WriteLine("\n=======================================\n");
 
-        KuliahMahasiswa1302210124 KuliahFadel = new KuliahMahasiswa1302210124();
-        KuliahFadel.ReadJSON();
+        KuliahMahasiswa1302210124 Kuliahryan = new KuliahMahasiswa1302210124();
+        Kuliahryan.ReadJSON();
     }
 }
