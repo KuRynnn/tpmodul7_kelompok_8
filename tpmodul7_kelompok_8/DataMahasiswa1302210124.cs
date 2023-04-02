87172c97a92a42a7a2cac3fcfe6a23f1fb1d173c
Drakkura <gonhgyhjj1@gmail.com>
2023-04-02 18:30:47 +0700

    menambahkan class DataMahasiswa1302210124

---
+++ tpmodul7_kelompok_8/DataMahasiswa1302210124.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace tpmodul7_kelompok_8
+{
+    internal class DataMahasiswa1302210124
+    {
+        public void ReadJSON()
+        {
+            String jsonString = File.ReadAllText("C:\\Users\\ryan\\Documents\\KPL\\tpmodul7_1302210124\\tpmodul7_kelompok_8\\tp7_1_1302210124.json");
+            ContohClassMahasiswa mahasiswa = JsonSerializer.Deserialize<ContohClassMahasiswa>(jsonString);
+
+            Console.WriteLine("Nama " + mahasiswa.nama.depan + " " + mahasiswa.nama.belakang +
+                " dengan nim " + mahasiswa.nim + " dari fakultas " + mahasiswa.fakultas);
+        }
+    }
+}
+
---
+++ tpmodul7_kelompok_8/nama.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul7_kelompok_8
+{
+    internal class nama
+    {
+        public string depan { get; set; }
+        public string belakang { get; set; }
+        public nama(string depan, string belakang)
+        {
+            this.depan = depan;
+            this.belakang = belakang;
+        }
+    }
+}
